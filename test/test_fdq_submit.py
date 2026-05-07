1bcb0ba8272a4b464e457dafd1bcd6917c953aac
marc <marc.stadelman@gmail.com>
2026-05-07 18:08:14 +0200

    dummy

--- test/test_fdq_submit.py
+++ test/test_fdq_submit.py
@@ -204,7 +204,7 @@ class TestFdqSubmit(unittest.TestCase):
                     "  account: account\n"
                     "  python_env_module: python/3.12\n"
                     "  uv_env_module: uv/0.6\n"
-                    "  fdq_version: 0.1.5\n"
+                    "  fdq_version: 0.1.5.dev2\n"
                     "  job_time: 1\n"
                     "mode:\n"
                     "  run_train: true\n"
